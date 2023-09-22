24ee3c017cb7710d4a38eff3a2ce0528ab93099a
AndyJones1989 <andrew.jones@parcel2go.com>
2023-09-22 15:33:42 +0100

    Template setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ marginTop: "25%" }}>Hi from Andy!</div>
+    </div>
+  );
+}
+
+export default App;
